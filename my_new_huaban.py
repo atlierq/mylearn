13c553f8a756fb18fe43c285ab8380636023ac49
atlierq <ahlijun1116@outlook.com>
2018-02-23 17:24:28 +0800

    try to use urlretrieve download method

--- my_new_huaban.py
+++ my_new_huaban.py
@@ -3,6 +3,8 @@ import re
 import json
 from m_common import *
 import urllib.parse as urlparse
+import urllib.request
+import os
 
 LIMIT=100
 
@@ -45,28 +47,41 @@ def getjson_data(url,**params):
 
 
 def extract_board(url):
-    json_data=getjson_data(url,limit=LIMIT)
-    title=json_data['title']
-    pin_list=json_data['pins']
-    pin_count=json_data['pin_count']
-    print(pin_count)
-    pin_count-=len(pin_list)
-    while pin_count >0:
-        json_data=getjson_data(url,limit=LIMIT,max=pin_list[-1]['pin_id'])
-        pins=json_data['pins']
-        pin_list +=pins
-        pin_count-=len(pins)
-    some_thing=list(map(Pin, pin_list))
-    return Board(title,some_thing )
+    try:
+        json_data=getjson_data(url,limit=LIMIT)
+        title=json_data['title']
+        pin_list=json_data['pins']
+        pin_count=json_data['pin_count']
+        print(pin_count)
+        pin_count-=len(pin_list)
+        while pin_count >0:
+            json_data=getjson_data(url,limit=LIMIT,max=pin_list[-1]['pin_id'])
+            pins=json_data['pins']
+            pin_list +=pins
+            pin_count-=len(pins)
+        some_thing=list(map(Pin, pin_list))
+        return Board(title,some_thing )
+    except:
+        print('something happended')
 
-
+def huaban_board_download(url,s_path):
+    urllib.request.urlretrieve(url,s_path)
 
 
 
 def main():
-    url='http://huaban.com/boards/13840182/'
+    url='http://huaban.com/boards/41233035/'
     board=extract_board(url)
+    print(board.title)
+    os.mkdir('d:\\image\\%s'%board.title)
+    # path='D:\image\\' + board.title+'\\'+x.id
+    # print(path)
     for x in board.pins:
-        print(x.url)
+        # path = 'D:\\image\\' + board.title + '\\'+x.id+'.'+x.ext
+        path='d:\\image\\%s\\%s.%s'%(board.title,x.id,x.ext)
+        huaban_board_download(x.url,path)
+        # print(path)
+        # print(x.url,x.id)
+        # D:\image
 
 main()
