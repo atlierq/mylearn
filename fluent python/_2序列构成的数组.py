ebbdf3d991d7a19955e07c4c9dffc4d211371a8d
atlierq <ahlijun1116@outlook.com>
2018-03-13 16:43:54 +0800

    aa

--- fluent python/_2序列构成的数组.py
+++ fluent python/_2序列构成的数组.py
@@ -106,40 +106,45 @@ NEEDLES = [0, 1, 2, 5, 8, 10, 22, 23, 29, 30, 31]
 for x in reversed(NEEDLES):
     print(x)
 #bisect 寻找数字位置
-import bisect
-import sys
-HAYSTACK = [1, 4, 5, 6, 8, 12, 15, 20, 21, 23, 23, 26, 29, 30]
-NEEDLES = [0, 1, 2, 5, 8, 10, 22, 23, 29, 30, 31]
-ROW_FMT = '{0:2d} @ {1:2d} {2}{0:<2d}'
-def demo(bisect_fn):
-    for needle in reversed(NEEDLES):
-        position = bisect_fn(HAYSTACK, needle)
-        offset = position * ' |'
-        print(ROW_FMT.format(needle, position, offset))
 
-if __name__ == '__main__':
-    if sys.argv[-1] == 'left':
-        bisect_fn = bisect.bisect_left
-    else:
-        bisect_fn = bisect.bisect
-        print('DEMO:', bisect_fn.__name__)
-        print('haystack ->', ' '.join('%2d' % n for n in HAYSTACK))
-        demo(bisect_fn)
-print("\n-------------------------\n")
+# import bisect
+# import sys
+# HAYSTACK = [1, 4, 5, 6, 8, 12, 15, 20, 21, 23, 23, 26, 29, 30]
+# NEEDLES = [0, 1, 2, 5, 8, 10, 22, 23, 29, 30, 31]
+# ROW_FMT = '{0:2d} @ {1:2d} {2}{0:<2d}'
+# def demo(bisect_fn):
+#     for needle in reversed(NEEDLES):
+#         position = bisect_fn(HAYSTACK, needle)
+#         offset = position * ' |'
+#         print(ROW_FMT.format(needle, position, offset))
+#
+# if __name__ == '__main__':
+#     if sys.argv[-1] == 'left':
+#         bisect_fn = bisect.bisect_left
+#     else:
+#         bisect_fn = bisect.bisect
+#         print('DEMO:', bisect_fn.__name__)
+#         print('haystack ->', ' '.join('%2d' % n for n in HAYSTACK))
+#         demo(bisect_fn)
+# print("\n-------------------------\n")
 
 #bisect插入数字
-import bisect
-import random
-size=7
-random.seed(129)
-my_list=[]
-for i in range(size):
-    new_item=random.randrange(size*2)
-    bisect.insort(my_list,new_item)
-    print('%2d->'%new_item,my_list)
-print("\n-------------------------\n")
+
+# import bisect
+# import random
+# size=7
+# random.seed(129)
+# my_list=[]
+# for i in range(size):
+#     new_item=random.randrange(size*2)
+#     bisect.insort(my_list,new_item)
+#     print('%2d->'%new_item,my_list)
+#
+# print("\n-------------------------\n")
 
 #2.9 当列表不是首选时
+aaa
+
 
 
 
