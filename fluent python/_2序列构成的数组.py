5f4d73755cbcf849c27183f78ad448344145715c
atlierq <ahlijun1116@outlook.com>
2018-03-21 14:06:06 +0800

    finish chapter 2

--- fluent python/_2序列构成的数组.py
+++ fluent python/_2序列构成的数组.py
@@ -1,3 +1,85 @@
+# 2.2.4 生成器表达式
+# symbols='@#$%^&*'
+# # print([ord(x) for x in symbols])
+# # print(list(filter(lambda c :c>40,map(ord,symbols))))
+# # colors=['a','b','c']
+# # sizes=[1,2]
+# # t=[(color,size) for color in colors
+# #                 for size in sizes]
+# # print(t)
+# #
+# # a1=tuple(ord(x) for x in symbols)
+# # print(a1)
+# # print(type(a1))
+#
+# # colors=['red','blue','white']
+# # sizes=['s','l','m']
+# # for x in ('%s %s' %(c,s) for c in colors for s in sizes):
+# #     print(x)
+#
+# #   _作占位符
+# import os
+# _,a=os.path.split(os.getcwd())
+# print(a)
+#
+# # *rest可以拆分可迭代对象,*rest可以放在任何位置
+# t=(1,2)
+# print(*t)
+# a,b,*rest=range(10)
+# print(a,rest)
+# print(a,*rest)
+#
+# #测试
+# # print('{:15}|{:^9}|{:^9}'.format('','ds','s'))
+#
+# #namedtuple  创建一个具名元组需要两个参数，一个是类名，另一个是类的各个字段的名字
+# from collections import namedtuple
+# City=namedtuple('City','name country population coordinates')
+# tokyo=City('tokyo','JP',369.33,(1635,1122))
+# # print(tokyo)
+# # print(tokyo.name,tokyo.coordinates)
+# # print(tokyo[-1])
+#
+# print(City._fields)
+# latlong=namedtuple('latlong','lat long')
+# a1=City('Delhi NCR', 'IN', 21.935, latlong(28.613889, 77.208889))
+# delhi_data = ('Delhi NCR', 'IN', 21.935, latlong(28.613889, 77.208889))
+# delhi=City._make(delhi_data)#等价于City(*delhi_data)
+# print(delhi)
+# a2=delhi._asdict()# _asdict() 把具名元组以 collections.OrderedDict 的形式返回，我们可以利用它来把元组里的信息友好地呈现出来。
+# print(a2)
+#
+# # for key,value in delhi._asdict().items():
+# #     print(key+':',value)
+
+
+#2.3.5 作为不可变列表的元组
+# a1=('a','b','c','d','a')
+# a2=(1,2,3)
+# b1=['a','b','c']
+# b2=[1,2,3]
+# print(a1.__add__(a2))
+# print(b1.__iadd__(a1))#tuple不可以使用，等价于b1+=a1
+# print(a1.__contains__('a'))
+# print(a1.count('a'))
+# print(a1.__getitem__(-1))
+# print(a1.index('b'))#在 s 中找到元素 e 第一次出现的位置
+# b1.insert(-1,'s')
+# # print(b1)
+# x=iter(a1)#使用iter()函数将Iterable变成Iterator
+# print(x)
+# for a in x:
+#     print(a)
+#from collections import Iterator
+# print(isinstance(x,Iterator))
+# print(next(x))
+# print(next(x))
+# print(next(x))
+# print(next(x))
+# print(sorted(a1))
+# print(b1)
+# b1.remove('b')#该方法没有返回值,so,不可以直接用print
+# print(b1)
 # a=list(range(5))
 # print(a)
 # print(a[:3])
@@ -168,10 +250,35 @@
 #数组排序需要重新建立一个数组
 
 #2.9.2 内存视图
+# import array
+# numbers = array.array('h', [-2, -1, 0, 1, 2])
+# memv = memoryview(numbers)
+# print(len(memv))
+# memv_oct=memv.cast('B')
+# print(memv_oct.tolist())
+
+# 2.9.3 NumPy和SciPy
+# import numpy
+# a=numpy.arange(12)
+# print(a)
+# print(a.shape)
+# a.shape=3,4
+# print(a)
+# print(a[:,1])#打印第二列
+# print(a.transpose())#行列转换
+
+# 示例 2-23 使用双向队列
+# from collections import deque
+# dq=deque(range(10),maxlen=(10))
+# print(dq)
+# dq.rotate(3)
+# print(dq)
+# dq.appendleft(100)
+# print(dq)
+# dq.extend([200,300])
+# print(dq)
+# print(dq.pop())
+# #deque不能用sort排序
 
 
-
-
-
-
-
+#（https://docs.python.org/3/howto/sorting.html）通过几个例子讲解了sorted 和 list.sort 的高级用法。
