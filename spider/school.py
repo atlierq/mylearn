90b7dc90123f7befc05119eb4529eefe955edd6f
atlierq <ahlijun1116@outlook.com>
2018-03-29 09:47:42 +0800

    use mysql

--- spider/school.py
+++ spider/school.py
@@ -2,6 +2,9 @@
 import requests
 from bs4 import BeautifulSoup
 from m_common import *
+import pymysql
+import traceback
+
 import re
 
 def getHtmlText(url,code='utf-8'):
@@ -29,15 +32,45 @@ def getSchoolurl(list):
             Stext=getHtmlText(url1,code='utf-8')
             Ssoup=BeautifulSoup(Stext,'html.parser')
             a=Ssoup.findAll('li',attrs={'class':'b_algo'})[0].a.attrs['href']
-
+            i=0
             while a.split('/')[2][-6:]!='edu.cn':
-                i=1
+                i+=1
                 a = Ssoup.findAll('li', attrs={'class': 'b_algo'})[i].a.attrs['href']
-                i=i+1
+
+
             school_url_list.update({x:a})
+
         except:
             continue
-    print(school_url_list)
+    return school_url_list
+
+def mysqlText(list):
+    conn = pymysql.connect(host='127.0.0.1', user='root', password='123456', db='school', charset="utf8")
+    cur=conn.cursor()
+    sqlc='''create table school(
+    id int(11) NOT NULL auto_increment PRIMARY KEY ,
+    school_name VARCHAR (255) NOT NULL ,
+    school_url VARCHAR (255) NOT NULL )DEFAULT charset=utf8;
+    '''
+    try:
+        cur.execute(sqlc)
+        conn.commit()
+        print('ok')
+    except:
+        traceback.print_exc()
+
+    for x in list:
+        sqla='''insert into school(school_name,school_url) VALUES (%s,%s);'''
+        try:
+            cur.execute(sqla,(x,list[x]))
+            conn.commit()
+            print('ok')
+        except:
+            traceback.print_exc()
+
+    conn.commit()
+    cur.close()
+
 
 
 
@@ -46,7 +79,8 @@ def main():
     html=getHtmlText(url)
     Schlist=[]
     getSchoollist(html,Schlist)
-    getSchoolurl(Schlist)
+    new_list=getSchoolurl(Schlist)
+    mysqlText(new_list)
 
 
 
